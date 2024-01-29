4c865e3fb73c6b65d849dfe6a62f2a8a02ee873b
RDnante <119560912+RDnante@users.noreply.github.com>
2024-01-29 19:43:49 +0600

    dateexp

--- src/main/java/com/example/ventevoiture/Config/JwtService.java
+++ src/main/java/com/example/ventevoiture/Config/JwtService.java
@@ -40,7 +40,7 @@ public class JwtService {
         .claims(extraClaims)
         .subject(userdetails.getUsername())
         .issuedAt(new Date(System.currentTimeMillis()))
-        .expiration(new Date(System.currentTimeMillis() + 1000 * 60 *(60 * 24)))
+        .expiration(new Date(System.currentTimeMillis() + 1000 * 60 *(60 * 120)))
         .signWith(getSignInKey(),Jwts.SIG.HS256)
         .compact();
     }
@@ -54,7 +54,7 @@ public class JwtService {
         return extractExpiration(token).before(new Date());
     }
 
-    private Date extractExpiration(String token) {
+    public Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
 
